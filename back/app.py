b42ed4a05d598e9017ce94d8d17cafc45c37e11e
MatheusFinatto <matheusfinatto2@gmail.com>
2025-02-17 16:34:13 -0300

    fix: added port and guniocorn

--- back/app.py
+++ back/app.py
@@ -5,8 +5,9 @@ import chess.polyglot
 import sys
 import os
 
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+parent_dir = os.path.dirname(os.path.abspath(__file__)) 
 sys.path.append(parent_dir)
+
 from common import get_best_move, DEPTH
 
 
@@ -54,4 +55,5 @@ def get_move():
     return jsonify({'move': move})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Usa a porta do Render ou 10000 como fallback
+    app.run(host="0.0.0.0", port=port, debug=False)
